0534e802ae361215ada29d84e10cf5b525da1267
DANIEL VALDEZ GUZMAN, BHD <daniel_valdezguzman@bhd.com.do>
2023-12-19 11:30:03 -0400

    fix

--- src/api/utils/mysql.connector.ts
+++ src/api/utils/mysql.connector.ts
@@ -11,12 +11,12 @@ let pool: any;
 export const init = async () => {
   try {
     pool = await createConnection({
-      connectionLimit: 4,
-      host: 'finantek-dev-api-do-user-15293740-0.c.db.ondigitalocean.com',
-      user: 'dev',
-      password: 'AVNS_1GCHLZawTjjpEwKiUdX',
-      database: 'finantek',
-      port: 25060
+      connectionLimit: DATA_SOURCES.mySqlDataSource.DB_CONNECTION_LIMIT,
+      host: DATA_SOURCES.mySqlDataSource.DB_HOST,
+      user: DATA_SOURCES.mySqlDataSource.DB_USER,
+      password: DATA_SOURCES.mySqlDataSource.DB_PASSWORD,
+      database: DATA_SOURCES.mySqlDataSource.DB_DATABASE,
+      port: +DATA_SOURCES.mySqlDataSource.DB_PORT
     });
 
     console.debug('MySql Adapter Pool generated successfully');
@@ -37,11 +37,11 @@ export const execute = async (query: string, params?: string[] | Object) => {
   try {
     await pool.beginTransaction();
     const [results] = await pool.execute(query, params);
-    await pool.commit();
+    // await pool.commit();
     return results;
 
   } catch (error) {
-    await pool.rollback();
+    // await pool.rollback();
     console.error('[mysql.connector][execute][Error]: ', error, query, params);
     throw new Error('failed to execute MySQL query');
   }
